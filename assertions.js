b6063529c1e2175dca4951d2f38d076722da792b
Michael McCool <michael.mccool@intel.com>
2018-07-20 14:50:56 +0900

    partial plan output

--- assertions.js
+++ assertions.js
@@ -12,17 +12,17 @@ var cheerio = require('cheerio');
 // Read in test specs and store as a map
 const ts_raw = fs.readFileSync(ts_htmlfile, 'UTF-8');
 var ts_dom = cheerio.load(ts_raw);
-var ts = {};
+var testspec = {};
 ts_dom('span[class="testspec"]').each(function(i,elem) {
     let id = ts_dom(this).attr('id');
     if (undefined === id) {
         console.log("Warning: testspec without id:",
                     ts_dom(this).html());
     } else {
-        ts[id] = ts_dom(this).text();;
+        testspec[id] = ts_dom(this).text();;
     }
 });
-console.log(ts);
+console.log(testspec);
 
 // Initialize plan dom with template
 const template_raw = fs.readFileSync(template_htmlfile, 'UTF-8');
@@ -32,7 +32,7 @@ var plan_dom = cheerio.load(template_raw);
 const src_raw = fs.readFileSync(src_htmlfile, 'UTF-8');
 var src_dom = cheerio.load(src_raw);
 
-// Extract assertions and merge with test specs into plan dom
+// Extract assertions
 var assertions = {};
 src_dom('span[class="rfc2119-assertion"]').each(function(i,elem) {
     let id = src_dom(this).attr('id');
@@ -44,6 +44,22 @@ src_dom('span[class="rfc2119-assertion"]').each(function(i,elem) {
     }
 });
 console.log(assertions);
+
+// Merge assertions and test specs into plan
+var plan_body = plan_dom('body');
+var plan_list = plan_body.append('<ul></ul>');
+for (a in assertions) {
+    console.log("Processing assertion "+a);
+    plan_elem = plan_list.append('<li></li>');
+    a_text = assertions[a];
+    plan_elem.append(a_text);
+    a_spec = testspec[a];
+    if (undefined === a_spec) {
+        console.log("Warning: no test spec for "+a);
+    } else {
+        plan_elem.append(a_spec);
+    }
+}
 
 // Output plan
 fs.writeFile(plan_htmlfile, plan_dom.html(), function(error) {
