5d66e596991b3eea98dd9feb929fdac39eeda7c4
Victor Charpenay <victor.charpenay@siemens.com>
2018-11-23 15:04:52 +0100

    added custom notes on class definitions. Final changes

--- render.sh
+++ render.sh
@@ -4,6 +4,9 @@ if ! [[ -e .install/fuseki.jar ]] ; then
 	mkdir .install
 	echo "Downloading RDF store (Apache Fuseki server)..."
 	curl http://repo1.maven.org/maven2/org/apache/jena/jena-fuseki-server/3.9.0/jena-fuseki-server-3.9.0.jar -o .install/fuseki.jar
+
+	echo "Installing Node.js dependencies..."
+	npm install
 fi
 
 echo "Starting RDF store (running on port 3030)..."
