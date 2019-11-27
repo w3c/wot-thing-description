0b727e4d7dce7c8e17933a2b19aaee4b0647336d
vcharpenay <vcharpenay@gmail.com>
2019-11-27 11:15:53 +0100

    fixed normative content in index.html template (no change in info model allowed); decoupling from RDF from now on

--- render.js
+++ render.js
@@ -73,16 +73,20 @@ const txtFiles = [
     'ontology/templates.txt'
 ];
 
-const src = fs.readFileSync('index.template.html', 'UTF-8');
-const jsonSchemaValidation = fs.readFileSync('validation/td-json-schema-validation.json', 'UTF-8');
-
-const atriskCSS = fs.readFileSync('testing/atrisk.css', 'UTF-8');
-
 const updateEndpoint = 'http://localhost:3030/temp'; // process.env.WOT_SPARUL_ENDPOINT;
 const queryEndpoint = 'http://localhost:3030/temp'; // process.env.WOT_SPARQL_ENDPOINT;
 if (!updateEndpoint) throw new Error('WOT_SPARUL_ENDPOINT not defined (SPARQL update endpoint)');
 if (!queryEndpoint) throw new Error('WOT_SPARQL_ENDPOINT not defined (SPARQL query endpoint)');
 
+//////////////////////////////////////////////////////// 0. render main document
+const src = fs.readFileSync('index.template.html', 'UTF-8');
+const atriskCSS = fs.readFileSync('testing/atrisk.css', 'UTF-8');
+const jsonSchemaValidation = fs.readFileSync('validation/td-json-schema-validation.json', 'UTF-8');
+
+let rendered = src.replace('{atriskCSS}', atriskCSS);
+rendered = rendered.replace('{td.json-schema.validation}', jsonSchemaValidation);
+
+fs.writeFileSync('index.html', rendered, 'UTF-8');
 ///////////////////////////////////////////////////////////// 1. clear endpoint
 load(updateEndpoint, null)
 ///////////////////////////////////// 2. generate 1.1 context file with nesting
@@ -136,67 +140,14 @@ load(updateEndpoint, null)
 
     return Promise.resolve();
 })
-//////////////////////////// 4. for each namespace, call HTML and DOT templates
+///////////////////////////////////// 4. for each namespace, call HTML templates
 .then(() => {
     sttl.connect(queryEndpoint);
-
-    let rendered = src;
 
     let td = { type: 'uri', value: 'https://www.w3.org/2019/wot/td#' };
     let jsonschema = { type: 'uri', value: 'https://www.w3.org/2019/wot/json-schema#' };
     let wotsec = { type: 'uri', value: 'https://www.w3.org/2019/wot/security#' };
     let hctl = { type: 'uri', value: 'https://www.w3.org/2019/wot/hypermedia#' };
-
-/*
-    // HTML rendering
-
-    let tpl1 = 'http://w3c.github.io/wot-thing-description/#classes';
-    sttl.callTemplate(tpl1, td)
-    .then(html => {
-        rendered = rendered.replace('{td}', html);
-        return sttl.callTemplate(tpl1, jsonschema);
-    })
-    .then((html) => {
-        rendered = rendered.replace('{json-schema}', html);
-        return sttl.callTemplate(tpl1, wotsec);
-    })
-    .then(html => {
-        rendered = rendered.replace('{wot-security}', html);
-        return sttl.callTemplate(tpl1, hyperm);
-    })
-    .then(html => {
-        rendered = rendered.replace('{hypermedia}', html);
-        return Promise.resolve();
-    })
-    .then(() => {
-        rendered = rendered.replace('{td.json-schema.validation}', jsonSchemaValidation);
-        rendered = rendered.replace('{atriskCSS}', atriskCSS);
-
-        fs.writeFileSync('index.html', rendered, 'UTF-8');
-    })
-    .catch(e => console.error('HTML rendering error: ' + e.message));
-
-    // DOT rendering
-
-    tpl2 = 'http://w3c.github.io/wot-thing-description/visualization#main';
-    sttl.callTemplate(tpl2, td)
-    .then(dot => {
-        fs.writeFileSync('visualization/td.dot', dot);
-        return sttl.callTemplate(tpl2, jsonschema);
-    })
-    .then(dot => {
-        fs.writeFileSync('visualization/json-schema.dot', dot);
-        return sttl.callTemplate(tpl2, wotsec);
-    })
-    .then(dot => {
-        fs.writeFileSync('visualization/wot-security.dot', dot);
-        return sttl.callTemplate(tpl2, hyperm);
-    })
-    .then(dot => {
-        fs.writeFileSync('visualization/hypermedia.dot', dot);
-    })
-    .catch(e => console.error('DOT rendering error: ' + e.message));
-*/
 
     // HTML rendering (ontology documents)
  
