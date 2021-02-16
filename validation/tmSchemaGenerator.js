caca171943592efaca2723870623ad2c61ea8227
Ege Korkan <egekorkan@gmail.com>
2021-02-16 12:39:50 +0100

    add enum removal

--- validation/tmSchemaGenerator.js
+++ validation/tmSchemaGenerator.js
@@ -8,10 +8,13 @@
  - Be recursive to be able to adapt to changes in the TD Schema
  Changes to the TD Schema:
  - Remove the term `required` from all levels
+ - Remove the term `enum` from all levels
+ - Maybe in the future: remove const but there is no use of it at the current state 
  - If a term is not of type string, allow also string
  - Adding TM specific link validation (not fully clear yet)
  Expectations:
- - Required: There are currently 21 required in the TD Schema. There should be 2 left that are objects 
+ - Required: There are currently 21 required in the TD Schema. There should be 2 left that are objects
+ - Enum: There are 29 enums, there should be 2 left.
 */
 
 
@@ -20,11 +23,10 @@ const fs = require('fs');
 // take the TD Schema
 let tdSchema = JSON.parse(fs.readFileSync('validation/td-json-schema-validation.json'));
 
-//console.log(tdSchema);
-
 let tdSchemaNoReq = removeRequired(tdSchema)
+let tdSchemaNoReqNoEnum = removeEnum(tdSchemaNoReq)
 
-fs.writeFileSync("validation/tm-json-schema-validation.json", JSON.stringify(tdSchemaNoReq,null,2))
+fs.writeFileSync("validation/tm-json-schema-validation.json", JSON.stringify(tdSchemaNoReqNoEnum,null,2))
 
 /** 
  * if there is a required, remove that
@@ -37,7 +39,7 @@ fs.writeFileSync("validation/tm-json-schema-validation.json", JSON.stringify(tdS
 function removeRequired(argObject) {
 
     // remove required if it exists and is of array type.
-    // check for array is needed since we also specify what a required is and that is an object
+    // check for array is needed since we also specify what a required is and that it is an object
     if (("required" in argObject) && (Array.isArray(argObject.required))){
         // need to decide whether to delete or replace it with ""
         // delete is "cleaner" but "" is more explicit
@@ -49,7 +51,39 @@ function removeRequired(argObject) {
         let curValue = argObject[key];
         // removal is done only in objects, other types are not JSON Schema points anyways
         if (typeof(curValue)=="object"){
-            argObject[key] = removeRequired(argObject[key])
+            argObject[key] = removeRequired(curValue)
+        }
+    } 
+
+    return argObject;
+}
+
+/** 
+ * if there is a enum, remove that
+ * once that is done, find a sub item that is of object type, call recursively
+ * if there is no sub item with object, return the current scoped object
+ * This is done to allow putting placeholders for a string that would be limited with
+ * a list of allowed values in an enum
+ * @param {object} argObject
+ * @return {object}
+**/
+
+function removeEnum(argObject) {
+
+    // remove enum if it exists and is of array type.
+    // check for array is needed since we also specify what a enum is and that it is an object
+    if (("enum" in argObject) && (Array.isArray(argObject.enum))){
+        // need to decide whether to delete or replace it with ""
+        // delete is "cleaner" but "" is more explicit
+        delete argObject.enum;
+    }
+
+    for (var key in argObject)
+    {
+        let curValue = argObject[key];
+        // removal is done only in objects, other types are not JSON Schema points anyways
+        if (typeof(curValue)=="object"){
+            argObject[key] = removeEnum(curValue)
         }
     } 
 
