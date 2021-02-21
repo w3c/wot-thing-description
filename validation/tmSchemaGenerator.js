7e0304700d8736c5704944217d1e90bf0ec3f7d1
Ege Korkan <egekorkan@gmail.com>
2021-02-21 19:03:20 +0100

    non working string converter

--- validation/tmSchemaGenerator.js
+++ validation/tmSchemaGenerator.js
@@ -25,24 +25,40 @@ const fs = require('fs');
 // take the TD Schema
 let tdSchema = JSON.parse(fs.readFileSync('validation/td-json-schema-validation.json'));
 
-let tmSchema = changeMetadata(tdSchema)
+let tmSchema = staticReplace(tdSchema)
 tmSchema = removeRequired(tmSchema)
 tmSchema = removeEnum(tmSchema)
 tmSchema = removeFormat(tmSchema)
+// tmSchema = convertString(tmSchema)
 
 fs.writeFileSync("validation/tm-json-schema-validation.json", JSON.stringify(tmSchema,null,2))
 
 /** 
- * This function changes the values of metadata terms in the schema.
- * These are title and description
+ * This function changes the values of terms in the schema in a static/deterministic way.
+ * These are title and description and `@type` in the root level
  * @param {object} argObject
  * @return {object}
 **/
-function changeMetadata(argObject){
+function staticReplace(argObject){
 
     argObject.title = "WoT Thing Model Schema - 16 February 2021"
     argObject.description = "JSON Schema for validating Thing Models. This is automatically generated from the WoT TD Schema."
-
+    argObject.properties["@type"] = {
+        "oneOf": [{
+                "type": "string",
+                "const":"ThingModel"
+            },
+            {
+                "type": "array",
+                "items": {
+                    "type": "string"
+                },
+                "contains":{
+                    "const":"ThingModel"
+                }
+            }
+        ]
+    }
     return argObject;
 }
 
@@ -137,4 +153,85 @@ function removeFormat(argObject) {
     } 
 
     return argObject;
+}
+
+/** 
+ * if there is a term that does not have `string` in the type key,
+ * convert that into an anyOf where one option is a string
+ * this allows the use of {{MY_PLACEHOLDER}} kind of placeholders that are always string
+ * If the term already is an anyOf or similar, it might be tricky to find an elegant option
+ * If the type is object or array, it is not touched, right? Is it allowed to have something like: "properties":"{{}}"
+ * @param {object} argObject
+ * @return {object}
+**/
+
+function convertString(argObject) {
+    console.log("---")
+    console.log(JSON.stringify(argObject))
+    // look for any type
+    if (("type" in argObject)){
+        //check if it is not a string and add it as anyOf
+        // console.log("there is type")
+        let curType = argObject.type;
+        // if((curType != "string") && (curType != "object") && (curType != "array")){
+        if(curType == "number"){
+            console.log(curType)
+            console.log(argObject)
+            process.exit(0)
+            delete argObject.type;
+            // this is the minimum
+            argObject.anyOf = [{
+                type: curType
+            },{
+                type: "string"
+            }]
+            console.log(argObject)
+        }
+
+    }
+    
+
+    // console.log(Object.keys(argObject))
+    
+    for (var key in argObject)
+    {
+        let curValue = argObject[key];
+        // removal is done only in objects, other types are not JSON Schema points anyways
+        if (typeof(curValue)=="object"){
+            if ("type" in curValue) {
+                if (curValue.type == "object"){
+                    // console.log(curValue)
+                    curValue = convertString(curValue)
+                    argObject[key] = curValue
+                }
+                else if (curValue.type == "array"){
+                    // think about iterating arrays as well
+                }
+            } else {
+                curValue = convertString(curValue)
+                argObject[key] = curValue
+            }
+
+        }
+        else {
+            // console.log(curValue);
+        }
+    } 
+    
+    return argObject;
+}
+
+/* 
+ Why the following wishful thinking will not work:
+ - If there is a maximum with the type number, it needs to be detected and put into the anyOf. So a string replacement is not the good approach
+*/
+
+
+function replaceWithString(argObject) {
+
+    argString = JSON.stringify(argObject);
+    // pick the types that will be replaced: number, integer, boolean
+    let resultString = argString.replace('"type":"number"','"anyOf":[{"type":"number"},{"type":"string"}]');
+    resultString = resultString.replace('"type":"integer"','"anyOf":[{"type":"number"},{"type":"string"}]');
+    return JSON.parse(resultString);
 }
