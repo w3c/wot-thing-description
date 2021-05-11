9f9561ef72bf1447aa1c8cabfc94cd377d28c97c
Victor Charpenay <victor.charpenay@fau.de>
2021-05-11 16:51:33 +0200

    fix ambiguous mapping for 'properties' in index map

--- context/merge.js
+++ context/merge.js
@@ -1,5 +1,7 @@
 let fs = require('fs');
 
+let cp = val => JSON.parse(JSON.stringify(val));
+
 let td = JSON.parse(fs.readFileSync('context/td-context.jsonld'));
 let jsonschema = JSON.parse(fs.readFileSync('context/json-schema-context.jsonld'));
 let wotsec = JSON.parse(fs.readFileSync('context/wot-security-context.jsonld'));
@@ -8,7 +10,15 @@ let hctl = JSON.parse(fs.readFileSync('context/hypermedia-context.jsonld'));
 let ctx = td['@context'];
 ctx['@version'] = 1.1;
 
-ctx.properties['@context'] = jsonschema['@context'];
+// if a nested context is defined with an index containers,
+// the index property is taken from the nested context, not its parent
+// see https://github.com/w3c/wot-thing-description/pull/1077
+let tmp = cp(jsonschema['@context']);
+let p = cp(tmp.properties);
+tmp.properties['@index'] = ctx.properties['@index'];
+tmp.properties['@context'] = { properties: p };
+ctx.properties['@context'] = tmp;
+
 ctx.input['@context'] = jsonschema['@context'];
 ctx.output['@context'] = jsonschema['@context'];
 ctx.data['@context'] = jsonschema['@context'];
