9e94f6af9770a07e82acdcda2d20b576b8729461
reluc <relu.cri@gmail.com>
2022-08-24 11:47:51 +0200

    chore: refactor and improve validation tests

---
+++ validation/resources/thing-descriptions.js
@@ -0,0 +1,51 @@
+const validTDs = [
+    {
+        "@context": [
+            "https://www.w3.org/2019/wot/td/v1",
+            "https://www.w3.org/2022/wot/td/v1.1",
+            {
+                "@language": "en"
+            }
+        ],
+        "title": "Simple Empty TD with Lang",
+        "description": "A simple empty TD, with just human readable text",
+        "support": "git://github.com/eclipse/thingweb.node-wot.git",
+        "version": { "instance": "0.5.1"},
+        "@type": "Thing",
+        "security": [
+            "nosec_sc"
+        ],
+        "id": "urn:uuid:55f01138-5c96-4b3d-a5d0-81319a2db677",
+        "securityDefinitions": {
+            "nosec_sc": {
+                "scheme": "nosec"
+            }
+        }
+    },
+    {
+        "@context": [
+            "https://www.w3.org/2019/wot/td/v1",
+            "https://www.w3.org/2022/wot/td/v1.1",
+        ],
+        "title": "Minimal TD",
+        "@type": "Thing",
+        "security": [
+            "nosec_sc"
+        ],
+        "id": "urn:uuid:55f01138-5c96-4b3d-a5d0-81319a2db677",
+        "securityDefinitions": {
+            "nosec_sc": {
+                "scheme": "nosec"
+            }
+        }
+    }
+   
+];
+const invalidTDs = [
+    
+];
+
+module.exports = {
+    validTDs,
+    invalidTDs
+};
---
+++ validation/td-validation.test.js
@@ -0,0 +1,100 @@
+/**
+ * Testing script that attempts to validate TD documents with:
+ *  - a JSON schema
+ *  - SHACL constraints (after transformation to RDF)
+ */
+'use strict';
+
+const fs = require('fs');
+const assert = require('assert');
+const { validTDs, invalidTDs } = require('./resources/thing-descriptions');
+const factory = require('rdf-ext')
+const ParserN3 = require('@rdfjs/parser-n3')
+const SHACLValidator = require('rdf-validate-shacl');
+const jsonld = require('jsonld');
+const Ajv = require("ajv");
+const { Readable } = require('stream');
+
+const tdSchema = fs.readFileSync("validation/td-json-schema-validation.json")
+const ajv = new Ajv({
+    strict: false,
+    addUsedSchema: false,
+    formats: {
+        // Do not validate the following formats.
+        // In the future we could provide a custom checking function for these formats
+        "uri-reference": true,
+        "json-pointer": true,
+        "uri": true,
+        "date-time": true,   
+       
+    }
+})
+
+describe('Thing Description validation', () => {
+    let shapes;
+
+    before(async () => {
+        const stream = fs.createReadStream('validation/td-validation.ttl')
+        shapes = await loadDatasetN3(stream)
+        
+        configureCustomLoader();
+    })
+
+    describe('JSONSchema validation', () => {
+        for (const [id, td] of validTDs.entries()) {
+            it(`should validate n° ${id}`, () => {
+                const valid = ajv.validate(JSON.parse(tdSchema), td)
+                assert.equal(valid, true, ajv.errorsText());
+            });
+        }
+    });
+
+    describe('SHACL validation', () => {
+        for (const [id, td] of validTDs.entries()) {
+            it(`should validate n° ${id}`, async () => {
+                const ttl = await jsonld.toRDF(td, { format: 'application/n-quads' })
+                const dataset = await loadDatasetN3(Readable.from([ttl]))
+                const validator = new SHACLValidator(shapes);
+                const report = await validator.validate(dataset);
+                assert.equal(report.conforms, true, prettyPrintSHACLValidationErrors(dataset,report));
+            });
+        }
+    });
+
+});
+
+function configureCustomLoader() {
+    const ctx = JSON.parse(fs.readFileSync('context/td-context-1.1.jsonld', 'utf-8'));
+    // define a mapping of context URL => context doc
+    const CONTEXTS = {
+        "https://www.w3.org/2022/wot/td/v1.1": ctx
+    };
+
+    // grab the built-in Node.js doc loader
+    const nodeDocumentLoader = jsonld.documentLoaders.node();
+
+    // change the default document loader
+    const customCtxLoader = async (url, options) => {
+        if (url in CONTEXTS) {
+            return {
+                contextUrl: null, // this is for a context via a link header
+                document: CONTEXTS[url], // this is the actual document that was loaded
+                documentUrl: url // this is the actual context URL after redirects
+            };
+        }
+        // call the default documentLoader
+        return nodeDocumentLoader(url);
+    };
+    jsonld.documentLoader = customCtxLoader;
+}
+
+async function loadDatasetN3(stream) {
+    const parser = new ParserN3({ factory })
+    return factory.dataset().import(parser.import(stream))
+}
+
+function prettyPrintSHACLValidationErrors(input,report) {
+    return "\n" + input.toCanonical() + "\n" + report.results.map(result => {
+        return `${result.message[0].value} at ${result.path}`
+    }).join('\n')
+}
